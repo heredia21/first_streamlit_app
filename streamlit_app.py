a4143319c91f2df41a46332cb5e76f634596b4f5
MH <16310744+heredia21@users.noreply.github.com>
2022-11-30 06:34:17 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.title ('Test')
-streamlit.head ('Breakfast Menu')
+streamlit.header ('Breakfast Menu')
 streamlit.text ('Hard-boiled free-range egg')
